d4112505d8b6c56f016c2b5fba226f8d2d68f91b
Justice <unankajustice@gmail.com>
2024-02-08 20:13:03 +0000

    Refactor bmiCalculator function and add compatibility calculation

--- index.js
+++ index.js
@@ -59,8 +59,6 @@ function bmiCalculator (weight, height){
   //bmi = Math.round(bmi);
       console.log('Your BMI is ' + bmi);
       return bmi;
-     
-
 }
 bmiCalculator(78, 163);
 
@@ -94,5 +92,18 @@ bmiCalculator(78, 163);
     * 27. Higher Order Function
     * 28. Pure Function
     * 29. Impure Function 
+    
  */
  
+// let n = Math.random();
+// n = n * 12
+// n= Math.floor(n) + 3;
+// console.log(n); 
+
+let nameOne = prompt("What is your name?");
+let nameTwo = prompt("What is their name?");
+  let n = Math.random();
+  n = n * 100;
+  n = Math.floor(n) + 1;
+alert(`${nameOne} and  ${nameTwo} ${n}% compartible `);
+
