23700c8a0d1b1d39ba95a3288205d880096a6055
Justice <unankajustice@gmail.com>
2024-02-17 13:49:44 +0000

    Update DOM elements and styles

--- document object model/index.js
+++ document object model/index.js
@@ -3,4 +3,8 @@
 var heading = document.firstElementChild.lastElementChild.firstChild;
 heading.innerHTML = "Good morning";
 document.querySelector("input").click();
-document.querySelector("ul").lastElementChild.innerHTML="Johnny";
+//document.querySelector("ul").lastElementChild.innerHTML="Johnny";
+document.getElementsByTagName("li")[2].style.color = "green";
+document.querySelector("li").style.fontSize = "300%";
+
+document.querySelector("button").style.backgroundColor = "yellow";
