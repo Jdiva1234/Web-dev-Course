af6d85bfcb1ba0af6f9b671782212b77f69d7fd4
Justice <unankajustice@gmail.com>
2024-02-19 22:32:23 +0000

    Fix draw message in index.js

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -11,5 +11,5 @@ document.querySelector("h1").innerHTML = "Player 1 wins";
 }else if(randomNumber2 > randomNumber1){
     document.querySelector("h1").innerHTML = "Player 2 wins";
 }else{
-    document.querySelector("h1").innerHTML="It is a draw, play again"
+    document.querySelector("h1").innerHTML="It is a draw; play again."
 }
