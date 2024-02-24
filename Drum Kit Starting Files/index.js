5660c06460ffe2eabe341cbfb302149c501df5a5
Justice <unankajustice@gmail.com>
2024-02-24 16:47:32 +0000

    Fix event handling in index.js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,13 +1,13 @@
 // loops, if are so important
 
+//i used the anonymous function, as you can see the funstion isn't defined
 
 for(let i =0; i< document.querySelectorAll(".drum").length; i++){
 
-document.querySelectorAll(".drum")[i].addEventListener("click", handleEvent)
+document.querySelectorAll(".drum")[i].addEventListener("click", function(){
 
-function handleEvent(){
-    alert("i just got clicked");
-}
+alert("I just got clicked")
+});
 
 }
 
