b10278af38ac959eb529accd0f946f0f9c51ea33
Justice <unankajustice@gmail.com>
2024-02-25 01:19:00 +0000

    Add functions for addition, multiplication, and calculator

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -8,9 +8,19 @@ document.querySelectorAll(".drum")[i].addEventListener("click", function(){
 
 alert("I just got clicked")
 });
-
 }
 
 
+//caling a function inside a function 
 
+function add(num1, num2){
+    return num1 + num2;
+}
 
+function multiply(num1, num2){
+    return num1 * num2
+}
+
+function calculator(num1, num2, operator){
+    return operator(num1, num2);
+}
