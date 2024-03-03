8ce7d1f1b86b1497f5ae9839064c513c69b0045e
Justice <unankajustice@gmail.com>
2024-03-03 16:07:35 +0000

    Add drum sound and image functionality

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -5,31 +5,76 @@
 for(let i =0; i< document.querySelectorAll(".drum").length; i++){
 
 document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var buttonInnerHTML = this.innerHTML;
+    
+    switch (buttonInnerHTML) {
+        case "w":
+            var audio = new Audio('/Drum Kit Starting Files/sounds/tom-1.mp3');
+           audio.play();
+            break;
+        case "a":
+            var audio = new Audio('/Drum Kit Starting Files/sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "s":
+             var audio = new Audio('/Drum Kit Starting Files/sounds/tom-3.mp3');
+             audio.play();
+    case "d":
+    var audio = new Audio('/Drum Kit Starting Files/sounds/tom-4.mp3');
+             audio.play();
+             break;
+        case "j":
+    var audio = new Audio('/Drum Kit Starting Files/sounds/snare.mp3');
+             audio.play();
+             break;
+         case "k":
+    var audio = new Audio('/Drum Kit Starting Files/sounds/kick-bass.mp3');
+             audio.play();
+             break;
+      case "l":
+    var audio = new Audio('/Drum Kit Starting Files/sounds/crash.mp3');
+             audio.play();
+             break;
+        default: console.log(buttonInnerHTML);
+            break
+    }
 
-alert("I just got clicked")
+
 });
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 //caling a function inside a function called higher order functions
 
-function add(num1, num2){
-    return num1 + num2;
-}
+// function add(num1, num2){
+//     return num1 + num2;
+// }
 
-function multiply(num1, num2){
-    return num1 * num2;
-}
+// function multiply(num1, num2){
+//     return num1 * num2;
+// }
  
- function substract(num1, num2){
-    return num1 - num2;
- }
+//  function substract(num1, num2){
+//     return num1 - num2;
+//  }
 
- function divide(num1, num2){
-    return num1 / num2;
- }
+//  function divide(num1, num2){
+//     return num1 / num2;
+//  }
 
 
-function calculator(num1, num2, operator){
-    return operator(num1, num2);
-}
+// function calculator(num1, num2, operator){
+//     return operator(num1, num2);
+// }
