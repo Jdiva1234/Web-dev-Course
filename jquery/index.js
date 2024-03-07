efde8c5b5007ee1836272a6dce2bf10be0f55028
Justice <unankajustice@gmail.com>
2024-03-07 09:59:24 +0000

    Add link to Grammarly and update button font size in index.html and index.js

--- jquery/index.js
+++ jquery/index.js
@@ -1,2 +1,3 @@
 $("h1").css("color","red");
-$("button").css("fontSize", 100);
+$("button").css("fontSize", 10);   //playing with the syles in JQuery
+$("a").attr("href",  "https://bootcamp.uxdesign.cc/5-6-ui-patterns-to-power-your-ai-products-ae1591981a78?gi=064232448b49&ref=dailydev") //playing JQuery attribute 
