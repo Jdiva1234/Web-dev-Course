1adee9a0d50731be0793a7dba14e06122c7cc5e9
Justice <unankajustice@gmail.com>
2024-03-07 10:20:58 +0000

    Update HTML, JS, and CSS files

--- jquery/index.js
+++ jquery/index.js
@@ -1,3 +1,13 @@
 $("h1").css("color","red");
 $("button").css("fontSize", 10);   //playing with the syles in JQuery
 $("a").attr("href",  "https://bootcamp.uxdesign.cc/5-6-ui-patterns-to-power-your-ai-products-ae1591981a78?gi=064232448b49&ref=dailydev") //playing JQuery attribute 
+
+$("button").click(function(){
+    $("h1").css("color","purple");
+})
+
+$(document).keypress(function(event){
+    $("h1").text(event.key); //this allows it to show on the hello section
+    $("button").text(event.key);
+    console.log(event.key); //this allows it to show on the console.
+});
