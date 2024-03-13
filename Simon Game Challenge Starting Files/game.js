cb5930c5353fb2a44c7110849d3f44e5bf49b262
Justice <unankajustice@gmail.com>
2024-03-13 06:48:15 +0000

    Refactor game.js: Fix typo and add event listener for button clicks

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,15 +1,19 @@
 var gamePattern = [];
-var buttonColours = ["red","blue","green","yellow"];
+var buttonColours = ["red", "blue", "green", "yellow"];
 
 function nextSequence() {
-   var randomNumber =  Math.floor(Math.random() * 4);
-    var randomChosenColour = buttonColours[randomNumber];
-     gamePattern.push(randomChosenColour);
+   var randomNumber = Math.floor(Math.random() * 4);
+   var randomChosenColour = buttonColours[randomNumber];
+   gamePattern.push(randomChosenColour);
 }
 
-$("#yellow").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
-function playAudio(){
-var audio = new Audio('/Simon Game Challenge Starting Files/sounds/yellow.mp3');
-audio.play();
-}
+$("#" + randomChosenColor).on("click", function () {
+ 
+      $("." + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100);
+ 
+      var audio = new Audio("/Simon Game Challenge Starting Files/sounds" + randomChosenColor + ".mp3");
+      audio.play();
+      return randomChosenColor;
+   });
+ 
+$(".btn").click(nextSequence);
