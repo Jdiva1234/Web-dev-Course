7155aa70850b1508dbfb4e07a351751a09648fb0
Justice <unankajustice@gmail.com>
2024-03-13 06:58:16 +0000

    Refactor game.js to improve code structure and readability

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,19 +1,53 @@
-var gamePattern = [];
-var buttonColours = ["red", "blue", "green", "yellow"];
+// var gamePattern = [];
+// var buttonColours = ["red", "blue", "green", "yellow"];
 
+// function nextSequence() {
+//    var randomNumber = Math.floor(Math.random() * 4);
+//    var randomChosenColour = buttonColours[randomNumber];
+//    gamePattern.push(randomChosenColour);
+// }
+
+// $("#" + randomChosenColor).on("click", function () {
+ 
+//       $("." + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100);
+ 
+//       var audio = new Audio("/Simon Game Challenge Starting Files/sounds" + randomChosenColor + ".mp3");
+//       audio.play();
+//       return randomChosenColor;
+//    });
+ 
+// $(".btn").click(nextSequence);
+
+
+
+// creates a array with the colors
+var buttonsColors = ["red", "blue", "green", "yellow"];
+ 
+// creates a game pattern
+var gamePatterns = [];
+ 
+// Creates a random number 0-3. Links the randon number with the colors. insert pattern in the game pattern
 function nextSequence() {
-   var randomNumber = Math.floor(Math.random() * 4);
-   var randomChosenColour = buttonColours[randomNumber];
-   gamePattern.push(randomChosenColour);
-}
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonsColors[randomNumber];
+  gamePatterns.push(randomChosenColor);
+ 
+  //On click select the element's ID and execute ne following function.
+ 
+  $("#" + randomChosenColor).on("click", function () {
+ 
+    // Select the corresponding CLASS e make it pulse.
+    $("." + randomChosenColor)
+      .fadeOut(100)
+      .fadeIn(100);
 
-$("#" + randomChosenColor).on("click", function () {
- 
-      $("." + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100);
- 
-      var audio = new Audio("/Simon Game Challenge Starting Files/sounds" + randomChosenColor + ".mp3");
-      audio.play();
-      return randomChosenColor;
-   });
- 
+      //added the sound 
+       var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+        audio.play();
+
+    return randomChosenColor;
+  });
+}
+// Triggers again the function after a click
 $(".btn").click(nextSequence);
+ 
