d0e04d6d53fed33b8c5de5dd7687d11a8504a4b9
Justice <unankajustice@gmail.com>
2024-06-23 14:57:48 +0100

    Refactor JavaScript code for better readability and structure

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const btnsShowModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal ');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+console.log(btnsShowModal);
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', function () {
+    console.log('button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+let closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
