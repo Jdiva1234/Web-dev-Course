898d32dea6e9773b622fe8ccdc3c74987afef4bd
Justice <unankajustice@gmail.com>
2024-06-23 19:30:44 +0100

    Refactor JavaScript code for better readability and structure

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+//selecting elements
+let score0 = document.querySelector('#score--0');
+let score1 = document.getElementById('score--1');
+let dice = document.querySelector('.dice');
+
+//starting conditions
+score0.textContent = 0;
+score1.textContent = 0;
+
+dice.classList.add('hidden');
