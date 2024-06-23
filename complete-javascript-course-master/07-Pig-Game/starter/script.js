2b934f3302e8c3389c72e69a298a7cdfecac76de
Justice <unankajustice@gmail.com>
2024-06-23 20:11:24 +0100

    chore: Refactor JavaScript code for better readability and structure

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -2,10 +2,32 @@
 //selecting elements
 let score0 = document.querySelector('#score--0');
 let score1 = document.getElementById('score--1');
+const current0 = document.getElementById('current--0');
+const current1 = document.getElementById('current--1');
 let dice = document.querySelector('.dice');
+let btnNew = document.querySelector('.btn--new');
+let btnRoll = document.querySelector('.btn--roll');
+let btnHold = document.querySelector('btn--hold');
 
 //starting conditions
 score0.textContent = 0;
 score1.textContent = 0;
-
 dice.classList.add('hidden');
+
+let currentScore = 0;
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //1. generating a random dice roll.
+  let diceNumber = Math.trunc(Math.random() * 6) + 1;
+
+  //2. Display dice
+  dice.classList.remove('hidden');
+  dice.src = `dice-${diceNumber}.png`;
+
+  //3. check for rolled 1:
+  if (dice !== 1) {
+    currentScore = currentScore + diceNumber;
+    current0.textContent = currentScore;
+  } else {
+  }
+});
