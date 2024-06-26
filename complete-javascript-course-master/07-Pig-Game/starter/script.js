64daee5d6600ef95621ea9ba8ff8680aa01ec102
Justice <unankajustice@gmail.com>
2024-06-26 22:06:34 +0100

    chore: Refactor JavaScript code for better readability and structure

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -9,12 +9,24 @@ let btnNew = document.querySelector('.btn--new');
 let btnRoll = document.querySelector('.btn--roll');
 let btnHold = document.querySelector('btn--hold');
 
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+const score = [0, 0];
+let activePlayer = 0;
+let currentScore = 0;
+
 //starting conditions
 score0.textContent = 0;
 score1.textContent = 0;
 dice.classList.add('hidden');
 
-let currentScore = 0;
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0.classList.toggle('player--active');
+  player1.classList.toggle('player--active');
+};
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   //1. generating a random dice roll.
@@ -25,9 +37,25 @@ btnRoll.addEventListener('click', function () {
   dice.src = `dice-${diceNumber}.png`;
 
   //3. check for rolled 1:
-  if (dice !== 1) {
+  if (diceNumber !== 1) {
     currentScore = currentScore + diceNumber;
-    current0.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    //switch to the next player
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  //1. add current score to active player's score
+
+  scores[activePlayer] += currentScore;
+
+  //2 check if player's score is 100
+  document.getElementById(`current--${activePlayer}`).textContent =
+    score[activePlayer];
+  //3. if so finish the game if not switch to the next player
+
+  switchPlayer();
 });
