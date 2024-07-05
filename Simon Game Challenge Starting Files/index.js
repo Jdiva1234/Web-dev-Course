07ffe81514f3f696135ee453f1dea52e99fe3ccf
Justice <unankajustice@gmail.com>
2024-07-05 10:15:07 +0100

    Understamding Hoisting and TDZ

---
+++ Simon Game Challenge Starting Files/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function calcAge(birthYear) {
+  const age = 2024 - birthYear;
+  console.log(firstName);
+  return age;
+
+  function add(a, b) {
+    return a + b;
+  }
+}
+console.log(add(2, 3));
+const firstName = 'Jonas';
+
+console.log(calcAge(1991));
